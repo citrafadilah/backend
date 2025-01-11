974f636241a1167e8ad2e1e9462199b1cdee7e13
Citra Fadilah Hasibuan <160827748+citrafadilah@users.noreply.github.com>
2025-01-12 00:03:02 +0700

    update

--- app-server/model/db.js
+++ app-server/model/db.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
-mongoose.connect(
-    
-    "mongodb+srv://mdp:haechan01y@cluster0.myncj.mongodb.net/dbbutik?retryWrites=true&w=majority&appName=Cluster0"
+
+//versi 2.2.12
+// "mongodb+srv://mdp:haechan01y@cluster0.myncj.mongodb.net/dbbutik?retryWrites=true&w=majority&appName=Cluster0"
+//  "mongodb://mdp:haechan01y@cluster0-shard-00-00.myncj.mongodb.net:27017,cluster0-shard-00-01.myncj.mongodb.net:27017,cluster0-shard-00-02.myncj.mongodb.net:27017/dbbutik?ssl=true&replicaSet=atlas-t07ebu-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+
+
+mongoose.connect(    
+    "mongodb://mdp:haechan01y@cluster0-shard-00-00.myncj.mongodb.net:27017,cluster0-shard-00-01.myncj.mongodb.net:27017,cluster0-shard-00-02.myncj.mongodb.net:27017/dbbutik?ssl=true&replicaSet=atlas-t07ebu-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
   ).then(() => {
     console.log("Connected to Database");
   }).catch((err) => {
--- app-server/routes/butiks.js
+++ app-server/routes/butiks.js
@@ -1,14 +1,16 @@
 var express = require("express");
 var router = express.Router();
+const checkAuth = require("../middleware/check-auth");
+
 
 const ButikController = require("../controller/butik");
 
-router.post("/", ButikController.createButik);
+router.post("/", checkAuth, ButikController.createButik);
 
-router.get("/", ButikController.getButiksByUser);
+router.get("/", checkAuth, ButikController.getButiksByUser);
 
-router.delete("/:id", ButikController.deleteButik);
+router.delete("/:id", checkAuth, ButikController.deleteButik);
 
-router.put("/:id", ButikController.updateButik);
+router.put("/:id", checkAuth, ButikController.updateButik);
 
 module.exports = router;
