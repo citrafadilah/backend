d66e78419adb46da3c399b56ffc1a6b8f4f4a108
Citra Fadilah Hasibuan <160827748+citrafadilah@users.noreply.github.com>
2025-01-11 23:44:37 +0700

    update

--- app.js
+++ app.js
@@ -32,14 +32,16 @@ app.use((req, res, next) => {
 // "mongodb+srv://mdp:haechan01y@cluster0.myncj.mongodb.net/dbbutik?retryWrites=true&w=majority&appName=Cluster0"
 //  "mongodb://mdp:haechan01y@cluster0-shard-00-00.myncj.mongodb.net:27017,cluster0-shard-00-01.myncj.mongodb.net:27017,cluster0-shard-00-02.myncj.mongodb.net:27017/dbbutik?ssl=true&replicaSet=atlas-t07ebu-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
 
-// mongoose.connect(
-//   "mongodb://mdp:haechan01y@cluster0-shard-00-00.myncj.mongodb.net:27017,cluster0-shard-00-01.myncj.mongodb.net:27017,cluster0-shard-00-02.myncj.mongodb.net:27017/dbbutik?ssl=true&replicaSet=atlas-t07ebu-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
-// ).then(() => {
-//   console.log("Connected to Database");
-// }).catch((err) => {
-//   console.error("App Starting error", err.stack);
-//   console.log("Connection Failed");
-// });
+// mongoose
+//   .connect(
+//     "mongodb://mdp:haechan01y@cluster0-shard-00-00.myncj.mongodb.net:27017,cluster0-shard-00-01.myncj.mongodb.net:27017,cluster0-shard-00-02.myncj.mongodb.net:27017/dbbutik?ssl=true&replicaSet=atlas-t07ebu-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+//   ).then(() => {
+//     console.log("Connected to Database");
+//   })
+//   .catch((err) => {
+//     console.error("App Starting error", err.stack);
+//     console.log("Connection Failed");
+//   });
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
