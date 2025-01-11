8e57351ca1ebf693d7dcd6b2c1ca72184238988f
Citra Fadilah Hasibuan <160827748+citrafadilah@users.noreply.github.com>
2025-01-12 00:36:03 +0700

    update

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 
 // mongoose
 //   .connect(
-//     "mongodb+srv://mdp:haechan01y@cluster0.myncj.mongodb.net/dbbutik?retryWrites=true&w=majority&appName=Cluster0"
+//     "mongodb://mdp:haechan01y@cluster0-shard-00-00.myncj.mongodb.net:27017,cluster0-shard-00-01.myncj.mongodb.net:27017,cluster0-shard-00-02.myncj.mongodb.net:27017/dbbutik?ssl=true&replicaSet=atlas-t07ebu-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
 //   ).then(() => {
 //     console.log("Connected to Database");
 //   })
