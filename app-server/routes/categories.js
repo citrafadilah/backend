cf5fa813059d338a9cd239ee6d96fc963ae96ee3
Citra Fadilah Hasibuan <160827748+citrafadilah@users.noreply.github.com>
2025-01-11 23:30:22 +0700

    update

--- app-server/routes/categories.js
+++ app-server/routes/categories.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+
 const CategoryController = require("../controller/categories");
 
 router.get("/", CategoryController.getCategories);
